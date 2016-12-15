030be3ea25a4d103ca515d898e95d33b849b89db
pengoor <runangaozhong@163.com>
2016-12-15 01:29:07 -0800

    [xprj, hikey] support hikey.

--- config.mk
+++ config.mk
@@ -10,7 +10,8 @@
 # accroding to your real board type.
 #
 #include ./configs/config_tiny210.mk
-include ./configs/config_bubblegum.mk
+#include ./configs/config_bubblegum.mk
+include ./configs/config_hikey.mk
 
 
 #
@@ -27,7 +28,7 @@ TOOLS_DIR=$(BUILD_DIR)/../tools
 
 BIT32_64=$(shell getconf LONG_BIT)
 
-ifeq ($(BOARD_NAME), bubblegum)
+ifeq ($(BOARD_ARCH), arm64)
 ifeq ($(BIT32_64), 64)
 COMPILE_URL=https://github.com/wowotechX/gcc-linaro-4.9-2015.02-3-x86_64_aarch64-linux-gnu.git
 CROSS_COMPILE=$(BUILD_DIR)/gcc-linaro-4.9-2015.02-3-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
@@ -37,7 +38,7 @@ CROSS_COMPILE=$(BUILD_DIR)/gcc-linaro-aarch64-linux-gnu-4.8-2013.12_linux/bin/aa
 endif
 endif
 
-ifeq ($(BOARD_NAME), tiny210)
+ifeq ($(BOARD_ARCH), arm)
 #use arm-linux-gcc-4.8.3 to build uboot and uboot-spl.
 COMPILE_URL=https://github.com/ooonebook/arm-none-linux-gnueabi-4.8.git
 CROSS_COMPILE=$(BUILD_DIR)/arm-none-linux-gnueabi-4.8/bin/arm-none-linux-gnueabi-
